6e6a2968681a6d8ecd3b88f765011fbb8fa7710a
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-05-16 17:09:50 +0900

    get_cost

--- report.sh
+++ report.sh
@@ -7,6 +7,51 @@ SHELL_DIR=$(dirname $0)
 USERNAME=${CIRCLE_PROJECT_USERNAME:-opsnow-tools}
 REPONAME=${CIRCLE_PROJECT_REPONAME:-valve-reporter}
 
+################################################################################
+
+TPUT=
+command -v tput > /dev/null && TPUT=true
+
+_echo() {
+    if [ "${TPUT}" != "" ] && [ "$2" != "" ]; then
+        echo -e "$(tput setaf $2)$1$(tput sgr0)"
+    else
+        echo -e "$1"
+    fi
+}
+
+_result() {
+    echo
+    _echo "# $@" 4
+}
+
+_command() {
+    echo
+    _echo "$ $@" 3
+}
+
+_success() {
+    echo
+    _echo "+ $@" 2
+    exit 0
+}
+
+_error() {
+    echo
+    _echo "- $@" 1
+    exit 1
+}
+
+_replace() {
+    if [ "${OS_NAME}" == "darwin" ]; then
+        sed -i "" -e "$1" $2
+    else
+        sed -i -e "$1" $2
+    fi
+}
+
+################################################################################
+
 prepare() {
     rm -rf ${SHELL_DIR}/build
     mkdir -p ${SHELL_DIR}/build
@@ -26,26 +71,27 @@ prepare() {
     fi
 }
 
+get_cost() {
+    aws ce get-cost-and-usage \
+        --granularity ${1} \
+        --time-period Start=${2},End=${3} \
+        --metrics "UnblendedCost" \
+        --filter ${4} \
+        | jq -r '"Start End Amount",
+                (.ResultsByTime[] | "\(.TimePeriod.Start) \(.TimePeriod.End) \(.Total.UnblendedCost.Amount)")' \
+        | column -t
+}
+
 get_cost_with_filter() {
     LIST=${SHELL_DIR}/build/filter
     ls ${SHELL_DIR}/config | grep json | sort > ${LIST}
 
     while read VAL; do
-        echo "Start=${PREV_MONTH},End=${NEXT_MONTH} UnblendedCost : ${VAL}"
-        aws ce get-cost-and-usage \
-            --time-period Start=${PREV_MONTH},End=${NEXT_MONTH} \
-            --granularity MONTHLY \
-            --metrics "UnblendedCost" \
-            --filter file://${SHELL_DIR}/config/${VAL} \
-            | jq '.ResultsByTime[] | {Start:.TimePeriod.Start,End:.TimePeriod.End,Amount:.Total.UnblendedCost.Amount}'
+        _result "Start=${PREV_MONTH},End=${NEXT_MONTH} UnblendedCost : ${VAL}"
+        get_cost "MONTHLY" ${PREV_MONTH} ${NEXT_MONTH} file://${SHELL_DIR}/config/${VAL}
 
-        echo "Start=${WEEK_BGN},End=${WEEK_END} UnblendedCost : ${VAL}"
-        aws ce get-cost-and-usage \
-            --time-period Start=${WEEK_BGN},End=${WEEK_END} \
-            --granularity DAILY \
-            --metrics "UnblendedCost" \
-            --filter file://${SHELL_DIR}/config/${VAL} \
-            | jq '.ResultsByTime[] | {Start:.TimePeriod.Start,End:.TimePeriod.End,Amount:.Total.UnblendedCost.Amount}'
+        _result "Start=${WEEK_BGN},End=${WEEK_END} UnblendedCost : ${VAL}"
+        get_cost "DAILY" ${WEEK_BGN} ${WEEK_END} file://${SHELL_DIR}/config/${VAL}
 
     done < ${LIST}
 }
@@ -63,21 +109,11 @@ get_cost_with_tag() {
 
             echo "{\"Tags\":{\"Key\":\"${TAG}\",\"Values\":[\"${VAL}\"]}}" > ${SHELL_DIR}/build/filter.json
 
-            echo "Start=${PREV_MONTH},End=${NEXT_MONTH} UnblendedCost : ${TAG}=${VAL}"
-            aws ce get-cost-and-usage \
-                --time-period Start=${PREV_MONTH},End=${NEXT_MONTH} \
-                --granularity MONTHLY \
-                --metrics "UnblendedCost" \
-                --filter file://${SHELL_DIR}/build/filter.json \
-                | jq '.ResultsByTime[] | {Start:.TimePeriod.Start,End:.TimePeriod.End,Amount:.Total.UnblendedCost.Amount}'
+            _result "Start=${PREV_MONTH},End=${NEXT_MONTH} UnblendedCost : ${TAG}=${VAL}"
+            get_cost "MONTHLY" ${PREV_MONTH} ${NEXT_MONTH} file://${SHELL_DIR}/build/filter.json
 
-            echo "Start=${WEEK_BGN},End=${WEEK_END} UnblendedCost : ${TAG}=${VAL}"
-            aws ce get-cost-and-usage \
-                --time-period Start=${WEEK_BGN},End=${WEEK_END} \
-                --granularity DAILY \
-                --metrics "UnblendedCost" \
-                --filter file://${SHELL_DIR}/build/filter.json \
-                | jq '.ResultsByTime[] | {Start:.TimePeriod.Start,End:.TimePeriod.End,Amount:.Total.UnblendedCost.Amount}'
+            _result "Start=${WEEK_BGN},End=${WEEK_END} UnblendedCost : ${TAG}=${VAL}"
+            get_cost "DAILY" ${WEEK_BGN} ${WEEK_END} file://${SHELL_DIR}/build/filter.json
 
         done < ${LIST}
 
